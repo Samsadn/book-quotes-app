9bc882a5603a7f9b1e3b3bdf11a0d66cd90f30cf
Samsadn <samsad.nahar01@gmail.com>
2025-11-19 19:00:10 +0100

    Added data

---
+++ Backend/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Book> Books => Set<Book>();
+    public DbSet<Quote> Quotes => Set<Quote>();
+}
